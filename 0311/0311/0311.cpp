fedbcabd0cc69fefb1b341d5c7fbecbf994b6e6d
wikim <unangel@yuhan.ac.kr>
2025-03-11 10:34:03 +0900

    add codes

---
+++ 0311/0311/0311.cpp
@@ -0,0 +1,12 @@
+﻿// 0311.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    /// std : standard 표준
+    /// :: ==> 범위 지정
+    /// cout : 사용할 수 있는 API/함수/기능
+    std::cout << "Hello World!\n";
+}
